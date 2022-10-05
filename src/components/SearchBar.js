3e703bc475b283aa904d95b20c1d97dfbeac9bb7
durotemy <durotemy@gmail.com>
2022-10-05 15:42:57 +0100

    navBar in progress

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from "@mui/icons-material";
+
+
+const SearchBar = () => {
+    return (
+        <Paper sx={{ color: 'red' }}>SearchBar</Paper>
+    )
+}
+
+export default SearchBar;
